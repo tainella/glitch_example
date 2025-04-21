f6d8c42e5ee409e10bed6b4d8ade80c722b4f73b
a.poleydobronravova <priklucheniye@mail.ru>
2025-04-21 18:46:18 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask (Амелия)"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
